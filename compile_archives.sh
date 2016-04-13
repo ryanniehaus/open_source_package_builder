555d856f0c96272b2982841f5e6ad8b1d89eb2e8
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-13 00:09:44 -0500

    updated travis script

--- compile_archives.sh
+++ compile_archives.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+wget https://github.com/ryanniehaus/heroku-bash-buildpack/raw/master/bin/dataURLFromCloudinary.py
 while IFS= read archiveLine
 do
   projectName=$(echo "$archiveLine" | cut -f 1 -d ",")
@@ -8,4 +9,7 @@ do
 	echo PROCESSING "$archiveLine" VERSION "$projectVersion"
 	wget "$archiveLocation"
 done < archives_to_process
+
+git add archives_to_process
+git commit -m "updating lists"
 
