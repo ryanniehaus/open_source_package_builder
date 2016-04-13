87b18de84ad0e3505960d2ac010c46dd945af7bd
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-13 08:00:43 -0500

    updating travis scripts

--- compile_archives.sh
+++ compile_archives.sh
@@ -4,6 +4,7 @@ wget https://github.com/ryanniehaus/heroku-bash-buildpack/raw/master/bin/dataURL
 chmod u+rx dataURLFromCloudinary.py
 
 sudo -H pip install --upgrade pip
+sudo -H pip install --upgrade ndg-httpsclient
 sudo -H pip install cloudinary
 
 eval "$(ssh-agent -s)"
