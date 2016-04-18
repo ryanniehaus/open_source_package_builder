51cc0f350d84cdbc6883b82721d61ffe6f080fbc
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-18 18:59:00 -0500

    added automated posting of release files after successful compile

--- compile_archives.sh
+++ compile_archives.sh
@@ -153,6 +153,24 @@ do
 				archiveSuccess=1
 			fi
 		popd > /dev/null
+		
+		if [ "$archiveSuccess" == "1" ]
+		then
+		  archiveSuccess=0
+		  echo "{" > releaseCreationRequest.json
+		  echo "  \"tag_name\": \"TESTRELEASE-1.0.0\"," >> releaseCreationRequest.json
+		  echo "  \"target_commitish\": \"master\"," >> releaseCreationRequest.json
+		  echo "  \"name\": \"TESTRELEASE-1.0.0\"," >> releaseCreationRequest.json
+		  echo "  \"body\": \"Description of the release\"," >> releaseCreationRequest.json
+		  echo "  \"draft\": false," >> releaseCreationRequest.json
+		  echo "  \"prerelease\": false" >> releaseCreationRequest.json
+		  echo "}" >> releaseCreationRequest.json
+		  
+		  curl -u "ryanniehaus:$GITHUB_PERSONAL_ACCESS_TOKEN" -i -v -X POST -d "$(cat releaseCreationRequest.json)" --header "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/ryanniehaus/open_source_package_builder/releases"
+		  uploadBaseURL=$(cat new_release_response.log | dos2unix | grep -E "^[[:space:]]+[\"]upload_url[\"]:[[:space:]]+[\"][^\"]+[\"],$" | sed 's|^[[:space:]]*[\"]upload_url[\"]:[[:space:]]*[\"]\([^\"]*\)[\"],$|\1|;s|{[^}]\+}$||')
+		  
+		  curl -1 -i -v -X POST -u "ryanniehaus:$GITHUB_PERSONAL_ACCESS_TOKEN" --data-binary "@$archiveFileName"  --header "Accept: application/vnd.github.v3+json" --header "Content-Type: application/gzip" "$uploadBaseURL?name=$archiveFileName&label=archive%20of%20source%20used%20to%20create%20build%20for%20$projectName%20v$projectVersion"
+		fi
 	
 	popd > /dev/null
 	rm -rf tempCompileDir
