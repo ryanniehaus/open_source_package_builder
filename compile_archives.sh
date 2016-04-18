9fb65113010224f8761a0c8efb0b2237fa0b06ac
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-17 22:16:12 -0500

    script dev

--- compile_archives.sh
+++ compile_archives.sh
@@ -161,8 +161,9 @@ done < archives_to_process
 git config --get remote.origin.url
 git remote set-url origin git@github.com:ryanniehaus/open_source_package_builder.git
 git remote show origin
-git fetch origin
-git update-ref HEAD master
+git remote set-branches --add origin master
+git fetch
+git checkout master
 mv archives_that_failed archives_to_process
 mv NEWarchives_successfully_processed archives_successfully_processed
 git add archives_to_process
