97743d830a1a6bf682d5a63e5af786c572d8b054
Ryan Niehaus <ryan.niehaus@gmail.com>
2016-04-19 21:14:03 -0500

    further fixes of compile_archives.sh

--- compile_archives.sh
+++ compile_archives.sh
@@ -235,12 +235,15 @@ do
 		  then
 				uploadBaseURL=$(cat new_release_response.log | dos2unix | grep -E "^[[:space:]]+[\"]upload_url[\"]:[[:space:]]+[\"][^\"]+[\"],$" | sed 's|^[[:space:]]*[\"]upload_url[\"]:[[:space:]]*[\"]\([^\"]*\)[\"],$|\1|;s|{[^}]\+}$||')
 				
-				curl -1 -X POST -u "ryanniehaus:$GITHUB_PERSONAL_ACCESS_TOKEN" --data-binary "@$archiveFileName"  --header "Accept: application/vnd.github.v3+json" --header "Content-Type: application/gzip" "$uploadBaseURL?name=$archiveFileName&label=archive%20of%20source%20used%20to%20create%20build%20for%20$projectName%20v$projectVersion"
-				curlRetval="$?"
-				if [ "$curlRetval" == "0" ]
-				then
-				  archiveSuccess=1
-				fi
+				for eachArchive in $(ls "$archiveFileName" *.tgz *.deb *.rpm)
+				do
+					curl -1 -X POST -u "ryanniehaus:$GITHUB_PERSONAL_ACCESS_TOKEN" --data-binary "@$eachArchive"  --header "Accept: application/vnd.github.v3+json" --header "Content-Type: application/gzip" "$uploadBaseURL?name=$archiveFileName&label=archive%20of%20source%20used%20to%20create%20build%20for%20$projectName%20v$projectVersion"
+					curlRetval="$?"
+					if [ "$curlRetval" == "0" ]
+					then
+						archiveSuccess=1
+					fi
+				done
 		  fi
 		fi
 	
